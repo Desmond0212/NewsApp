68ac40d0e7fa75faea18d7f6d9ed5e5d98ce26aa
Desmond Wong <desmond.9602@gmail.com>
2020-03-19 22:56:15 +0800

    Initial Commit

---
+++ newsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newsApp
+//
+//  Created by Desmond Wong on 19/03/2020.
+//  Copyright © 2020 Desmond Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
